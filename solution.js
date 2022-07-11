633333bcc443e04901e18fa9d2d3089d98174834
Leo Volwahsen <volwahsenl@gmail.com>
2022-07-11 11:55:31 +0200

    lets go

---
+++ solution.js
@@ -0,0 +1,48 @@
+// //1
+// for (x = 1; x <= 20; x++) {
+//     console.log("1.", x);
+// }
+
+// //2
+// for (let beer = 1; beer < 6; beer++) {
+//     if (beer === 1) {
+//         console.log("There is ", beer, " bottle of beer on the wall.");
+//     } else if (beer >= 2) {
+//         console.log("There are ", beer, " bottles of beer on the wall");
+//     }
+// }
+
+// //3
+// for (let count = 0; count < 21; count++) {
+//     if (count % 2 === 0) {
+//         console.log(count, " is even");
+//     } else {
+//         console.log(count, " is odd");
+//     }
+// }
+
+//4
+
+for (let value = 0; value <= 10; value++) {
+    console.log(value * 9);
+}
+
+for (let one = 1; one <= 10; one++) {
+    for (let second = 1; second <= 10; second++) {
+        console.log(one * second);
+    }
+}
+
+//5
+for (let two = 1; two <= 100; two++) {
+    if (two % 3 === 0) {
+        console.log("Fizz");
+    } else if (two % 5 === 0) {
+        console.log("Buzz");
+    } else if (two % 3 === 0 && two % 5 === 0);
+    {
+        console.log("FizzBuzz");
+    }
+}
+
+//
